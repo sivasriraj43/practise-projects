bff01c29042f12febe6a1086d02f89c5230feae7
sivasriraj43 <shivasriraj43@gmail.com>
2024-09-09 16:27:10 +0530

    account created in E-commerce

---
+++ E-commerce/account/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import PasswordInput,TextInput
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
+
+    def __init__(self,*args,**kwargs):
+        super(CreateUserForm,self).__init__(*args,**kwargs)
+
+        self.fields['email'].required=True
+
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        if User.objects.filter(email = email).exists():
+            raise forms.ValidationError("This email is already registered")
+        if len(email)>=250:
+            raise forms.ValidationError("Your email is too long.. ")
+        return email
+    
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(widget=TextInput())
+    password=forms.CharField(widget=PasswordInput())
+
+#update form
+class UpdateUserForm(forms.ModelForm):
+
+    password=None
+    class Meta:
+        model = User
+        fields=['username','email']
+        exclude=['password1','password2']
