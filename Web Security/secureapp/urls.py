079308fe77468637dd5dee3f08a2fe7f471ab8f9
sivasriraj43 <shivasriraj43@gmail.com>
2024-09-14 17:57:40 +0530

    USERAPP created

---
+++ Web Security/secureapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.Home,name='Home'),
+    path('register/',views.register,name='register'),
+    path('dashboard',views.dashboard,name='dashboard'),
+]
--- Web Security/secureapp/views.py
+++ Web Security/secureapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+    
+    return render(request,'index.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
