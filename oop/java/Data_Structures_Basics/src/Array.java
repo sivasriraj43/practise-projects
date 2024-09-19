164a348f8a5bf9c9704629fe10a75dcad0cb3fbc
sivasriraj43 <shivasriraj43@gmail.com>
2024-09-19 14:46:49 +0530

    class view changing

--- oop/java/Data_Structures_Basics/src/Array.java
+++ oop/java/Data_Structures_Basics/src/Array.java
@@ -1,7 +1,18 @@
 public class Array {
+	//array
     public static void main(String[] args) {
         
-        int[] arr ={1,2,3,4,5};
+        //int[] arr ={1,2,3,4,5};
+        int []arr = new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        
+        for(int i=0;i<arr.length;i++) {
+        	System.out.println("Element: "+arr[i]);
+        }
 
         System.out.println(arr[3]);
 
